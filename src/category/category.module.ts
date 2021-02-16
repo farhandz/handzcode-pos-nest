1dd77e25735dde5279c01e92326155152baae804
farhandz <ammarjoz09@gmail.com>
2021-02-16 15:11:39 +0700

    fix err

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -3,9 +3,11 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { categorySchema } from '../model/category.schema';
+import { UserModule } from 'src/user/user.module';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Category', schema: categorySchema }]),
+    UserModule,
   ],
   providers: [CategoryService],
   controllers: [CategoryController],
