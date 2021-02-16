972bdc5622b77b5a65ade7de8add14ba8d038224
farhandz <ammarjoz09@gmail.com>
2021-02-16 15:06:57 +0700

    add

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -6,10 +6,13 @@ import {
   Put,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { Observable, from } from 'rxjs';
 import { Category } from './category.interface';
+import { JwtAuthGuard } from 'src/guard/jwt-guard';
+import { RolesGuard } from 'src/guard/roles.guard';
 
 @Controller('category')
 export class CategoryController {
@@ -18,10 +21,15 @@ export class CategoryController {
   getDataCategory(): Observable<Category[]> {
     return from(this.categoryService.getData());
   }
+
+  // admin only can access this method
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Post('')
   addDataCategory(@Body() category: Category): Observable<Category> {
     return from(this.categoryService.insertData(category));
   }
+  // admin only can access this method
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Put(':id')
   editData(
     @Param('id') id: string,
@@ -29,6 +37,8 @@ export class CategoryController {
   ): Observable<Category> {
     return from(this.categoryService.editData(id, category));
   }
+  // admin only can access this method
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Delete(':id')
   DeleteData(@Param('id') id: string): Observable<any> {
     return from(this.categoryService.DeleteData(id));
