b174507099517ce550f1f36d0d4c84272ff07651
farhandz <ammarjoz09@gmail.com>
2021-02-18 09:55:32 +0700

    mongodb migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,22 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
 import { HistoryModule } from './history/history.module';
+import { UserModule } from './user/user.module';
 import { TrackingResiModule } from './tracking-resi/tracking-resi.module';
 import { EmployeModule } from './employe/employe.module';
 import { DiscountModule } from './discount/discount.module';
 import { CategoryModule } from './category/category.module';
+import * as dotenv from 'dotenv';
+
+// setup doten
+dotenv.config();
 
 @Module({
   imports: [
     AuthModule,
     ProductModule,
-    MongooseModule.forRoot('mongodb://localhost/handposbe'),
+    MongooseModule.forRoot(process.env.DATABASE_ACCESS),
     UserModule,
     HistoryModule,
     TrackingResiModule,
