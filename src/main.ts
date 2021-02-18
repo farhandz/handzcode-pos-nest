6d5866ebd1d5774e25cc3426b73c568b1acda06f
farhandz <ammarjoz09@gmail.com>
2021-02-18 11:41:16 +0700

    aaa

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
